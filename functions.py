1bdf4d00ef76b5f65219d5408a561eeebcab9bd9
Hang <cheerchung@gmail.com>
2021-07-07 17:44:08 +0200

    ipynb file

---
+++ functions.py
@@ -0,0 +1,492 @@
+# Library of Functions
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import sys
+import gc
+
+def display_factorial_planes(X_projected, pca, axis_ranks, alpha=0.8,
+                             illustrative_var=None, zoom=0.9):
+    '''Display a scatter plot on a factorial plane, with points colored
+    depending on values of illustrative_var.
+    Illustrative_var types: cluster(array) or feature(Series)
+    axis_rank: [(0,1)] for 1 graphe, or [(0,1),(1,2)] for 2 graphe'''
+
+    # For each factorial plane
+    for d1, d2 in axis_ranks:
+        # Initialization
+        fig, ax = plt.subplots(figsize=(10, 8))
+        var = np.array(illustrative_var)
+        # Display the points
+        if var is None:
+            ax.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
+        # colored by cluster
+        elif len(np.unique(var)) < 11:
+            for value in np.unique(var):
+                mask = np.where(var == value)
+                ax.scatter(X_projected[mask, d1], X_projected[mask, d2],
+                           alpha=alpha, label=value)
+            ax.legend()
+        # colored by feature values
+        else:
+            im = ax.scatter(X_projected[:, d1], X_projected[:, d2],
+                            alpha=alpha, c=illustrative_var.values,
+                            cmap='RdYlGn')
+            cbar = fig.colorbar(im, ax=ax)
+            cbar.set_label(illustrative_var.name, rotation=90)
+
+        # Define the limits of the chart
+        boundary = np.max(np.abs(X_projected[:, [d1, d2]])) / zoom
+        ax.set(xlim=(-boundary, boundary), ylim=(-boundary, boundary))
+
+        # Display grid lines
+        ax.plot([-100, 100], [0, 0], color='grey', ls='--')
+        ax.plot([0, 0], [-100, 100], color='grey', ls='--')
+        
+        if pca != None:
+            # Label the axes, with the percentage of variance explained
+            ax.set_xlabel(
+                'PC{} ({:.2%})'.format(d1 + 1, pca.explained_variance_ratio_[d1]))
+            ax.set_ylabel(
+                'PC{} ({:.2%})'.format(d2 + 1, pca.explained_variance_ratio_[d2]))
+            ax.set_title(
+                "Projection of points (on PC{} and PC{})".format(d1 + 1, d2 + 1))
+        else:
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+            ax.set_title('Projection of points via TSNE')
+
+def snake_plot(X, cluster):
+    '''This function gives snake plot of different cluster'''
+    sns.set_theme()
+    plt.figure(figsize=(15, 5))
+    data_K_snake = X.assign(Cluster=cluster)
+    data_K_snake = pd.melt(data_K_snake, id_vars=['Cluster'],
+                           var_name='Metric')
+    plt.title('Snake plot of normalized variables')
+    plt.xlabel('Metric')
+    plt.ylabel('Value')
+    sns.lineplot(
+        data=data_K_snake, x='Metric', y='value', hue='Cluster', ci=0,
+        palette='RdYlGn')
+    plt.show()
+
+
+def gini(labels_):
+    '''This function calculates the Gini coefficient of cluster.labels_,which
+    is between 0 and 1. 0 indicates homogeneously distributed clusters.
+    1 indicates most elements are in one cluster.'''
+    _, array = np.unique(labels_, return_counts=True)
+    array = np.sort(array)
+    index = np.arange(1, array.shape[0]+1)
+    n = array.shape[0]
+    return (np.sum((2 * index - n - 1) * array)) / (n * np.sum(array))
+
+
+def radar_plot(df_scaled, vars: list, ax, label=None, frac=1, color='m'):
+
+    '''This function draw a radar plot. df_scaled is scaled dataframe with
+    cluster labels. Vars is the list of varibles choosed to display.
+    Labels decides which cluster to draw.'''
+
+    # select variables to display
+    df_radar = df_scaled.groupby('cluster')[vars].mean()
+    # cluster name and size
+    size = (df_scaled['cluster'] == label).sum()
+    # axis setting of radar plot
+    xticks = vars
+    n_vars = len(vars)
+    angles = np.linspace(0, 2 * np.pi, n_vars + 1)
+
+    # plot a radar plot for label cluster
+    values = df_radar.loc[label, :].values.tolist()
+    # match dimension of angles
+    values += values[:1]
+    ax.plot(angles, values, linewidth=2, label='test', c=color)
+    ax.fill(angles, values, alpha=0.2, c=color)
+
+    # axis setting of plot
+    ax.set_title('Cluster_{}: {} customers'.format(label, size), pad=10,
+                 c=color, fontdict={'fontsize': 14})
+    # set position of first elements in angles at the top
+    ax.set_theta_offset(np.pi / 2)
+    # set clockwise plot
+    ax.set_theta_direction(-1)
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(xticks)
+    return ax
+
+
+def kde_target(df, var_name, target='TARGET'):
+    
+    '''Plots the disribution of a variable colored by value of the target'''
+
+    # Calculate the correlation coefficient between the new variable and the target
+    corr = df[target].corr(df[var_name])
+    # Calculate medians for repaid vs not repaid
+    avg_repaid = df.loc[df[target] == 0, var_name].median()
+    avg_not_repaid = df.loc[df[target] == 1, var_name].median()
+    
+    plt.figure(figsize = (12, 6))
+    
+    # Plot the distribution for target == 0 and target == 1
+    sns.kdeplot(df.loc[df[target] == 0, var_name], label = 'TARGET == 0')
+    sns.kdeplot(df.loc[df[target] == 1, var_name], label = 'TARGET == 1')
+    
+    # label the plot
+    plt.xlabel(var_name); plt.ylabel('Density')
+    plt.title('%s Distribution' % var_name)
+    plt.legend()
+    
+    # print out the correlation
+    print('The correlation between %s and the TARGET is %0.4f' % (var_name, corr))
+    # Print out average values
+    print('Median value for loan that was not repaid = %0.4f' % avg_not_repaid)
+    print('Median value for loan that was repaid =     %0.4f' % avg_repaid)
+
+def agg_numeric(df, parent_var, df_name):
+    """
+    Groups and aggregates the numeric values in a child dataframe
+    by the parent variable.
+    
+    Parameters
+    --------
+        df (dataframe): 
+            the child dataframe to calculate the statistics on
+        parent_var (string): 
+            the parent variable used for grouping and aggregating
+        df_name (string): 
+            the variable used to rename the columns
+        
+    Return
+    --------
+        agg (dataframe): 
+            a dataframe with the statistics aggregated by the `parent_var` for 
+            all numeric columns. Each observation of the parent variable will have 
+            one row in the dataframe with the parent variable as the index. 
+            The columns are also renamed using the `df_name`. Columns with all duplicate
+            values are removed. 
+    
+    """
+    
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != parent_var and 'SK_ID' in col:
+            df = df.drop(columns = col)
+            
+    # Only want the numeric variables
+    parent_ids = df[parent_var].copy()
+    numeric_df = df.select_dtypes('number').copy()
+    numeric_df[parent_var] = parent_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(parent_var).agg(['count', 'mean', 'max', 'min', 'sum'])
+
+    # Need to create new column names
+    columns = []
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        if var != parent_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+    
+    agg.columns = columns
+    
+    # Remove the columns with all redundant values
+    _, idx = np.unique(agg, axis = 1, return_index=True)
+    agg = agg.iloc[:, idx]
+    
+    return agg
+
+
+def target_corrs(df):
+    '''List of correlations'''
+    corrs = []
+
+    # Iterate through the columns 
+    for col in df.columns:
+        print(col)
+        # Skip the target column
+        if col != 'TARGET':
+            # Calculate correlation with the target
+            corr = df['TARGET'].corr(df[col])
+
+            # Append the list as a tuple
+            corrs.append((col, corr))
+            
+    # Sort by absolute magnitude of correlations
+    corrs = sorted(corrs, key = lambda x: abs(x[1]), reverse = True)
+    
+    return corrs
+
+def agg_categorical(df, parent_var, df_name):
+    """
+    Aggregates the categorical features in a child dataframe
+    for each observation of the parent variable.
+    
+    Parameters
+    --------
+    df : dataframe 
+        The dataframe to calculate the value counts for.
+        
+    parent_var : string
+        The variable by which to group and aggregate the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+        
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+    
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with aggregated statistics for each observation of the parent_var
+        The columns are also renamed and columns with duplicate values are removed.
+        
+    """
+    
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('category'))
+
+    # Make sure to put the identifying id on the column
+    categorical[parent_var] = df[parent_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(parent_var).agg(['sum', 'count', 'mean'])
+    
+    column_names = []
+    
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['sum', 'count', 'mean']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+    
+    categorical.columns = column_names
+    
+    # Remove duplicate columns by values
+    _, idx = np.unique(categorical, axis = 1, return_index = True)
+    categorical = categorical.iloc[:, idx]
+    
+    return categorical
+
+# Function to calculate missing values by column# Funct 
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+
+def return_size(df):
+    """Return size of dataframe in gigabytes"""
+    return round(sys.getsizeof(df) / 1e9, 2)
+
+
+def convert_types(df, print_info = False):
+    
+    original_memory = df.memory_usage().sum()
+    
+    # Iterate through each column
+    for c in df:
+        
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)
+            
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')
+        
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)
+        
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)
+            
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+        
+    new_memory = df.memory_usage().sum()
+    
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+        
+    return df
+
+def remove_missing_columns(train, test, threshold = 90):
+    # Calculate missing stats for train and test (remember to calculate a percent!)
+    train_miss = pd.DataFrame(train.isnull().sum())
+    train_miss['percent'] = 100 * train_miss[0] / len(train)
+    
+    test_miss = pd.DataFrame(test.isnull().sum())
+    test_miss['percent'] = 100 * test_miss[0] / len(test)
+    
+    # list of missing columns for train and test
+    missing_train_columns = list(train_miss.index[train_miss['percent'] > threshold])
+    missing_test_columns = list(test_miss.index[test_miss['percent'] > threshold])
+    
+    # Combine the two lists together
+    missing_columns = list(set(missing_train_columns + missing_test_columns))
+    
+    # Print information
+    print('There are %d columns with greater than %d%% missing values.' % (len(missing_columns), threshold))
+    
+    # Drop the missing columns and return
+    train = train.drop(columns = missing_columns)
+    test = test.drop(columns = missing_columns)
+    
+    return train, test
+
+def aggregate_client(df, group_vars, df_names):
+    """Aggregate a dataframe with data at the loan level 
+    at the client level
+    
+    Args:
+        df (dataframe): data at the loan level
+        group_vars (list of two strings): grouping variables for the loan 
+        and then the client (example ['SK_ID_PREV', 'SK_ID_CURR'])
+        names (list of two strings): names to call the resulting columns
+        (example ['cash', 'client'])
+        
+    Returns:
+        df_client (dataframe): aggregated numeric stats at the client level. 
+        Each client will have a single row with all the numeric data aggregated
+    """
+    
+    # Aggregate the numeric columns
+    df_agg = agg_numeric(df, parent_var = group_vars[0], df_name = df_names[0])
+    
+    # If there are categorical variables
+    if any(df.dtypes == 'category'):
+    
+        # Count the categorical columns
+        df_counts = agg_categorical(df, parent_var = group_vars[0], df_name = df_names[0])
+
+        # Merge the numeric and categorical
+        df_by_loan = df_counts.merge(df_agg, on = group_vars[0], how = 'outer')
+
+        gc.enable()
+        del df_agg, df_counts
+        gc.collect()
+
+        # Merge to get the client id in dataframe
+        df_by_loan = df_by_loan.merge(df[[group_vars[0], group_vars[1]]], on = group_vars[0], how = 'left')
+
+        # Remove the loan id
+        df_by_loan = df_by_loan.drop(columns = [group_vars[0]])
+
+        # Aggregate numeric stats by column
+        df_by_client = agg_numeric(df_by_loan, parent_var = group_vars[1], df_name = df_names[1])
+
+        
+    # No categorical variables
+    else:
+        # Merge to get the client id in dataframe
+        df_by_loan = df_agg.merge(df[[group_vars[0], group_vars[1]]], on = group_vars[0], how = 'left')
+        
+        gc.enable()
+        del df_agg
+        gc.collect()
+        
+        # Remove the loan id
+        df_by_loan = df_by_loan.drop(columns = [group_vars[0]])
+        
+        # Aggregate numeric stats by column
+        df_by_client = agg_numeric(df_by_loan, parent_var = group_vars[1], df_name = df_names[1])
+        
+    # Memory management
+    gc.enable()
+    del df, df_by_loan
+    gc.collect()
+
+    return df_by_client
+
+def plot_feature_importances(df, threshold = 0.9):
+    """
+    Plots 15 most important features and the cumulative importance of features.
+    Prints the number of features needed to reach threshold cumulative importance.
+    
+    Parameters
+    --------
+    df : dataframe
+        Dataframe of feature importances. Columns must be feature and importance
+    threshold : float, default = 0.9
+        Threshold for prining information about cumulative importances
+        
+    Return
+    --------
+    df : dataframe
+        Dataframe ordered by feature importances with a normalized column (sums to 1)
+        and a cumulative importance column
+    
+    """
+    
+    plt.rcParams['font.size'] = 18
+    
+    # Sort features according to importance
+    df = df.sort_values('importance', ascending = False).reset_index()
+    
+    # Normalize the feature importances to add up to one
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+    df['cumulative_importance'] = np.cumsum(df['importance_normalized'])
+
+    # Make a horizontal bar chart of feature importances
+    plt.figure(figsize = (10, 6))
+    ax = plt.subplot()
+    
+    # Need to reverse the index to plot most important on top
+    ax.barh(list(reversed(list(df.index[:15]))), 
+            df['importance_normalized'].head(15), 
+            align = 'center', edgecolor = 'k')
+    
+    # Set the yticks and labels
+    ax.set_yticks(list(reversed(list(df.index[:15]))))
+    ax.set_yticklabels(df['feature'].head(15))
+    
+    # Plot labeling
+    plt.xlabel('Normalized Importance'); plt.title('Feature Importances')
+    plt.show()
+    
+    # Cumulative importance plot
+    plt.figure(figsize = (8, 6))
+    plt.plot(list(range(len(df))), df['cumulative_importance'], 'r-')
+    plt.xlabel('Number of Features'); plt.ylabel('Cumulative Importance'); 
+    plt.title('Cumulative Feature Importance');
+    plt.show();
+    
+    importance_index = np.min(np.where(df['cumulative_importance'] > threshold))
+    print('%d features required for %0.2f of cumulative importance' % (importance_index + 1, threshold))
+    
+    return df
